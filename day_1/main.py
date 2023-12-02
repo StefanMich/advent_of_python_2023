12163785e29e71ff8da99c62b8dba741084e1b2f
Stefan Micheelsen <sm@intempus.dk>
2023-12-02 14:56:05 +0100

    Part 2 solution

--- day_1/main.py
+++ day_1/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from dataclasses import dataclass
 import os
 import sys
+from typing import Optional
 
 
 def part_1():
@@ -26,6 +28,63 @@ def part_1():
         calibration_value += value
     return calibration_value
 
+numbers = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+}
+
+
+@dataclass
+class Location:
+    index: int
+    value: Optional[int] = None
+
+
+def part_2():
+
+    with open('input') as f:
+        lines = f.readlines()
+
+    calibration_value = 0
+
+    for line in lines:
+
+        left = Location(index=sys.maxsize)
+        right = Location(index=-1)
+
+        for number_str, number in numbers.items():
+            start = 0
+            while (index := line.find(number_str, start)) != -1:
+                if index < left.index:
+                    left.index = index
+                    left.value = number
+                if index > right.index:
+                    right.index = index
+                    right.value = number
+                start = index + 1
+
+        print(line, left.value, right.value)
+        value = 10 * left.value + right.value
+        calibration_value += value
+    return calibration_value
+
 
 if __name__ == '__main__':
     print(part_1())
+    print(part_2())
