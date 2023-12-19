435b20607f1b67c8ffa6acc7a5521893ab63f92e
Stefan Micheelsen <sm@intempus.dk>
2023-12-19 20:23:02 +0100

    Day 5 part 2 not working

--- day_5/main.py
+++ day_5/main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from itertools import zip_longest
 
 
 class Range:
@@ -13,33 +14,38 @@ class Range:
 
 class RangedMap:
     def __init__(self) -> None:
-        self.ranges: list[Range] = list()
+        self.ranges: list[Range] = [Range(0, 0, 0)]
 
     def __getitem__(self, item: int) -> int:
-        for r in self.ranges:
-            if item > r.source:
-                if item <= r.source + r.range:
-                    return r.destination + (item - r.source)
+        range_with_next = zip_longest(self.ranges, self.ranges[1:])
+        # manger range der starter fra 0
+        for range, next_range in range_with_next:
+            if next_range is None or range.source <= item < next_range.source:
+                if item <= range.source + range.range:
+                    return range.destination + (item - range.source)
+                else:
+                    return item
         return item
 
     def add(self, range: Range) -> None:
+        if range.source == 0:
+            self.ranges.pop(0)
         self.ranges.append(range)
         self.ranges.sort(key=lambda x: x.source)
 
 
-def part_1() -> int:
-    with open('input') as f:
-        lines = f.readlines()
+def get_seed_numbers(lines: list[str]) -> list[int]:
     seeds = lines.pop(0)
     _, seed_numbers_string = seeds.split(':')
     seed_numbers: list[int] = list(map(int, seed_numbers_string.strip().split(' ')))
-
     lines.pop(0)
+    return seed_numbers
 
+
+def build_maps(lines: list[str]) -> list[RangedMap]:
     maps = list()
     _map = RangedMap()
     maps.append(_map)
-
     for line in lines:
         if ':' in line:
             continue
@@ -50,6 +56,15 @@ def part_1() -> int:
 
         destination, source, range = tuple(map(int, line.strip().split(' ')))
         _map.add(Range(destination, source, range))
+    return maps
+
+
+def part_1() -> int:
+    with open('input') as f:
+        lines: list[str] = f.readlines()
+    seed_numbers = get_seed_numbers(lines)
+
+    maps = build_maps(lines)
 
     locations = list()
     for seed in seed_numbers:
@@ -60,5 +75,34 @@ def part_1() -> int:
     return min(locations)
 
 
+def part_2() -> int:
+    with open('input') as f:
+        lines = f.readlines()
+    seed_numbers: list[int] = get_seed_numbers(lines)
+
+    maps = build_maps(lines)
+
+    seed_ranges: list[tuple[int, int]] = []
+    while seed_numbers:
+        start, _range = seed_numbers[:2]
+        seed_numbers = seed_numbers[2:]
+        seed_ranges.append((start, _range))
+
+    locations = list()
+
+    for start, _range in seed_ranges:
+        print(f'starting range from {start} to {start + _range}')
+        seed_number = start
+        range_end = start + _range
+        while seed_number < range_end:
+            result = seed_number
+            for _map in maps:
+                result = _map[result]
+            locations.append(result)
+            seed_number += 1
+    return min(locations)
+
+
 if __name__ == '__main__':
-    print(part_1())
+    #print(part_1())
+    print(part_2())
